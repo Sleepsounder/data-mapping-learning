553cd06a37e0fba27da7df75e4c059361e20b24b
Geoff Register <geoff.register@smashingboxes.com>
2019-08-01 09:40:50 -0400

    first commit

---
+++ Rutgers-data-mapping.rb
@@ -0,0 +1,547 @@
+@data = 
+[{
+  :patient_id=>10190, 
+  :order_provider=>"dbell58", 
+  :order_name=>"RPR (RAPID PLASMA REAGIN), SERUM", 
+  :lab_date=>"6/6/2019", 
+  :lab_ord_dtl=>"3503    RPR REFLEX TO TP-PA", 
+  :lab_analyte=>"RPR", 
+  :lab_order_type=>"RPR (RAPID PLASMA REAGIN), SERUM", 
+  :lab_value=>"NON-REACTIVE", 
+  :order_id=>587617, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"RPR", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10190, 
+  :order_provider=>"dbell58", 
+  :order_name=>"RPR (RAPID PLASMA REAGIN), SERUM", 
+  :lab_date=>"6/6/2019", 
+  :lab_ord_dtl=>"3503    RPR REFLEX TO TP-PA", 
+  :lab_analyte=>"RPR TITER", 
+  :lab_order_type=>"RPR (RAPID PLASMA REAGIN), SERUM", 
+  :lab_value=>"NOT INDIC. TITER", 
+  :order_id=>587617, :order_src=>"Visit", 
+  :lab_order_genus=>"RPR", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10190, 
+  :order_provider=>"dbell58", 
+  :order_name=>"CT + NG RNA, PCR, UNSPECIFIED SPECIMEN", 
+  :lab_date=>"6/6/2019", 
+  :lab_ord_dtl=>"4112    GC AND CHLAMYDIA BY TMA,URINE", 
+  :lab_analyte=>"CHLAMYDIA BY TMA", 
+  :lab_order_type=>"CT + NG RNA, PCR, UNSPECIFIED SPECIMEN", 
+  :lab_value=>"NEGATIVE", 
+  :order_id=>587613, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"CHLAMYDIA/GC", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10190, 
+  :order_provider=>"dbell58", 
+  :order_name=>"CT + NG RNA, PCR, UNSPECIFIED SPECIMEN", 
+  :lab_date=>"6/6/2019", 
+  :lab_ord_dtl=>"4112    GC AND CHLAMYDIA BY TMA,URINE", 
+  :lab_analyte=>"GONORRHEA BY TMA", 
+  :lab_order_type=>"CT + NG RNA, PCR, UNSPECIFIED SPECIMEN", 
+  :lab_value=>"NEGATIVE", 
+  :order_id=>587613, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"CHLAMYDIA/GC", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10190, 
+  :order_provider=>"dbell58", 
+  :order_name=>"HIV-1 RNA, QUANTITATIVE, PCR, SERUM OR PLASMA", 
+  :lab_date=>"6/6/2019", 
+  :lab_ord_dtl=>"4141    HIV-1 QUANT, PCR", 
+  :lab_analyte=>"HIV-1 VIRAL COPY", 
+  :lab_order_type=>"HIV-1 RNA, QUANTITATIVE, PCR, SERUM OR PLASMA", 
+  :lab_value=>"<20 COPIES/ML", 
+  :lab_interpretation=>"Abnormal", 
+  :order_id=>587616, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"HIV", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10190, 
+  :order_provider=>"dbell58", 
+  :order_name=>"HIV-1 RNA, QUANTITATIVE, PCR, SERUM OR PLASMA", 
+  :lab_date=>"6/6/2019", 
+  :lab_ord_dtl=>"4141    HIV-1 QUANT, PCR", 
+  :lab_analyte=>"HIV-1 VIRAL LOG", 
+  :lab_order_type=>"HIV-1 RNA, QUANTITATIVE, PCR, SERUM OR PLASMA", 
+  :lab_value=>"<1.301 LOG COPIES/ML", 
+  :lab_interpretation=>"Abnormal",
+  :order_id=>587616, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"HIV", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10192, 
+  :order_provider=>"nroscoe1", 
+  :order_name=>"RPR (RAPID PLASMA REAGIN), SERUM", 
+  :lab_date=>"6/25/2019", 
+  :lab_ord_dtl=>"3503    RPR REFLEX TO TP-PA", 
+  :lab_analyte=>"RPR", 
+  :lab_order_type=>"RPR (RAPID PLASMA REAGIN), SERUM", 
+  :lab_value=>"NON-REACTIVE", 
+  :order_id=>609460, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"RPR", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10192, 
+  :order_provider=>"nroscoe1", 
+  :order_name=>"RPR (RAPID PLASMA REAGIN), SERUM", 
+  :lab_date=>"6/25/2019", 
+  :lab_ord_dtl=>"3503    RPR REFLEX TO TP-PA", 
+  :lab_analyte=>"RPR TITER", 
+  :lab_order_type=>"RPR (RAPID PLASMA REAGIN), SERUM", 
+  :lab_value=>"NOT INDIC. TITER", 
+  :order_id=>609460, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"RPR", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10192, 
+  :order_provider=>"nroscoe1", 
+  :order_name=>"HIV (1+2) ANTIBODIES, EIA, SERUM, REFLEX HIV-1 WESTERN BLOT (WB)", 
+  :lab_date=>"6/25/2019", 
+  :lab_ord_dtl=>"3540    HIV 1/2 4TH GEN, RFLX CONF", 
+  :lab_analyte=>"HIV 1/2 4TH GEN, RFLX CONF", 
+  :lab_order_type=>"HIV (1+2) ANTIBODIES, EIA, SERUM, REFLEX HIV-1 WESTERN BLOT (WB)", 
+  :lab_value=>"NON-REACTIVE", 
+  :order_id=>609446, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"HIV 1 HIV 2 AB", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10192, 
+  :order_provider=>"nroscoe1", 
+  :order_name=>"CT + NG DNA, PCR, URINE", 
+  :lab_date=>"6/25/2019", 
+  :lab_ord_dtl=>"4335    GC AND CHLAMYDIA, AMPLIFIED, URINE", 
+  :lab_analyte=>"CHLAMYDIA, TMA", 
+  :lab_order_type=>"CT + NG DNA, PCR, URINE", 
+  :lab_value=>"NEGATIVE", 
+  :order_id=>609445, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"CHLAMYDIA/GC", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10192, 
+  :order_provider=>"nroscoe1", 
+  :order_name=>"CT + NG DNA, PCR, URINE", 
+  :lab_date=>"6/25/2019", 
+  :lab_ord_dtl=>"4335    GC AND CHLAMYDIA, AMPLIFIED, URINE", 
+  :lab_analyte=>"GONORRHEA, TMA", 
+  :lab_order_type=>"CT + NG DNA, PCR, URINE", 
+  :lab_value=>"NEGATIVE", 
+  :order_id=>609445, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"CHLAMYDIA/GC", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10192, 
+  :order_provider=>"nroscoe1", 
+  :order_name=>"CT + NG DNA, PCR, URINE", 
+  :lab_date=>"6/26/2019", 
+  :lab_ord_dtl=>"4335    GC AND CHLAMYDIA, AMPLIFIED, URINE", 
+  :lab_analyte=>"CHLAMYDIA, TMA", 
+  :lab_order_type=>"CT + NG DNA, PCR, URINE", 
+  :lab_value=>"NEGATIVE", 
+  :order_id=>609445, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"CHLAMYDIA/GC", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>10192, 
+  :order_provider=>"nroscoe1", 
+  :order_name=>"CT + NG DNA, PCR, URINE", 
+  :lab_date=>"6/26/2019", 
+  :lab_ord_dtl=>"4335    GC AND CHLAMYDIA, AMPLIFIED, URINE", 
+  :lab_analyte=>"GONORRHEA, TMA", 
+  :lab_order_type=>"CT + NG DNA, PCR, URINE", 
+  :lab_value=>"NEGATIVE", 
+  :order_id=>609445, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"CHLAMYDIA/GC", 
+  :lab_status=>"Final"
+}, 
+{
+  :patient_id=>13195, 
+  :order_provider=>"nroscoe1", 
+  :order_name=>"CT + NG DNA, PCR, URINE", 
+  :lab_date=>"6/26/2019", 
+  :lab_ord_dtl=>"4335    GC AND CHLAMYDIA, AMPLIFIED, URINE", 
+  :lab_analyte=>"CHLAMYDIA, TMA", 
+  :lab_order_type=>"CT + NG DNA, PCR, URINE", 
+  :lab_value=>"NEGATIVE", 
+  :order_id=>609445, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"CHLAMYDIA/GC", 
+  :lab_status=>"Final"
+},
+{
+  :patient_id=>13195, 
+  :order_provider=>"nroscoe1", 
+  :order_name=>"CT + NG DNA, PCR, URINE", 
+  :lab_date=>"6/26/2019", 
+  :lab_ord_dtl=>"4335    GC AND CHLAMYDIA, AMPLIFIED, URINE", 
+  :lab_analyte=>"GONORRHEA, TMA", 
+  :lab_order_type=>"CT + NG DNA, PCR, URINE", 
+  :lab_value=>"NEGATIVE", 
+  :order_id=>609445, 
+  :order_src=>"Visit", 
+  :lab_order_genus=>"CHLAMYDIA/GC", 
+  :lab_status=>"Final"
+}]
+
+
+@desired_output = 
+[{
+  :patient_id=>10190,
+  :date=>"6/6/2019", 
+  :lab1=>"RPR (RAPID PLASMA REAGIN), SERUM, 3503    RPR REFLEX TO TP-PA, RPR (RAPID PLASMA REAGIN), SERUM", 
+  :lab1_result=>"NON-REACTIVE", 
+  :lab2=>"RPR (RAPID PLASMA REAGIN), SERUM, 3503    RPR REFLEX TO TP-PA, RPR (RAPID PLASMA REAGIN), SERUM",
+  :lab2_result=>"NOT INDIC. TITER", 
+  :lab3=>"CT + NG RNA, PCR, UNSPECIFIED SPECIMEN, 4112    GC AND CHLAMYDIA BY TMA,URINE, CT + NG RNA, PCR, UNSPECIFIED SPECIMEN",
+  :lab3_result=>"NEGATIVE", 
+  :lab4=>"CT + NG RNA, PCR, UNSPECIFIED SPECIMEN, 4112    GC AND CHLAMYDIA BY TMA,URINE, CT + NG RNA, PCR, UNSPECIFIED SPECIMEN",
+  :lab4_result=>"NEGATIVE", 
+  :lab5=>"HIV-1 RNA, QUANTITATIVE, PCR, SERUM OR PLASMA, 4141    HIV-1 QUANT, PCR, HIV-1 RNA, QUANTITATIVE, PCR, SERUM OR PLASMA", 
+  :lab5_result=>"<20 COPIES/ML", 
+  :lab6=>"HIV-1 RNA, QUANTITATIVE, PCR, SERUM OR PLASMA, 4141    HIV-1 QUANT, PCR, HIV-1 RNA, QUANTITATIVE, PCR, SERUM OR PLASMA",
+  :lab6_result=>"<1.301 LOG COPIES/ML"
+}, 
+{
+  :patient_id=>10190,
+  :date=>"6/25/2019", 
+  :lab1=>"RPR (RAPID PLASMA REAGIN), SERUM, 3503    RPR REFLEX TO TP-PA, RPR (RAPID PLASMA REAGIN), SERUM",
+  :lab1_result=>"NON-REACTIVE",
+  :lab2=>"RPR (RAPID PLASMA REAGIN), SERUM, 3503    RPR REFLEX TO TP-PA, RPR (RAPID PLASMA REAGIN), SERUM",
+  :lab2_result=>"NOT INDIC. TITER",
+  :lab3=>"HIV (1+2) ANTIBODIES, EIA, SERUM, REFLEX HIV-1 WESTERN BLOT (WB), 3540    HIV 1/2 4TH GEN, RFLX CONF, HIV (1+2) ANTIBODIES, EIA, SERUM, REFLEX HIV-1 WESTERN BLOT (WB)",
+  :lab3_result=>"NON-REACTIVE",
+  :lab4=>"CT + NG DNA, PCR, URINE, 4335    GC AND CHLAMYDIA, AMPLIFIED, URINE, CT + NG DNA, PCR, URINE", 
+  :lab4_result=>"NEGATIVE",
+  :lab5=>"CT + NG DNA, PCR, URINE, 4335    GC AND CHLAMYDIA, AMPLIFIED, URINE, CT + NG DNA, PCR, URINE", 
+  :lab5_result=>"NEGATIVE", 
+}, 
+{
+  :patient_id=>10192,
+  :date=>"6/26/2019", 
+  :lab1=>"CT + NG DNA, PCR, URINE, 4335    GC AND CHLAMYDIA, AMPLIFIED, URINE, CT + NG DNA, PCR, URINE",  
+  :lab1_result=>"NEGATIVE",
+  :lab2=>"CT + NG DNA, PCR, URINE, 4335    GC AND CHLAMYDIA, AMPLIFIED, URINE, CT + NG DNA, PCR, URINE", 
+  :lab2_result=>"NEGATIVE" 
+},
+{
+:patient_id=>13195,
+:date=>"6/26/2019", 
+:lab1=>"CT + NG DNA, PCR, URINE, 4335    GC AND CHLAMYDIA, AMPLIFIED, URINE, CT + NG DNA, PCR, URINE",  
+:lab1_result=>"NEGATIVE",
+:lab2=>"CT + NG DNA, PCR, URINE, 4335    GC AND CHLAMYDIA, AMPLIFIED, URINE, CT + NG DNA, PCR, URINE", 
+:lab2_result=>"NEGATIVE" 
+}]
+
+def new_data(data)
+    new_array = []
+    data.each do |h|
+        new_hash = {}
+        number = 1
+        if h.patient_id && h.lab_date == new_array.any
+            h.order_name key should be renamed "lab#{number ++}" 
+            and "lab#{number}"s value shoud be "#{h.order_name} + #{h.lab_order_detail} + #{h.lab_order_type}"
+            add h.lab_value renamed "lab#{number}_result" 
+        elsif h.patient_id and h.lab_date to new_array with h.order_name key renamed to be "lab1" 
+            and "lab1"s value shoud be "#{h.order_name} + #{h.lab_order_detail} + #{h.lab_order_type}"
+            add h.lab_value renamed "lab#{number}_result"
+        else
+            h.patient_id && h.lab_date do not match new_array.patient_id && new_array.lab_date
+            new_array << new_hash 
+            new_hash.clear
+        end
+    end
+    new_array
+end
+
+puts @data
+
+
+
+# repl.it progress
+data = [{
+    :patient_id=>10, 
+    :lab_date=>"6/6/2019",
+    :lab_value=>"NON-REACTIVE",
+    :lab_order_genus=>"RPR",
+  },
+  {
+    :patient_id=>10, 
+    :lab_date=>"6/6/2019",
+    :lab_value=>"different!",
+    :lab_order_genus=>"qqq",
+  }, 
+    {
+    :patient_id=>10, 
+    :lab_date=>"6/6/2019",
+    :lab_value=>"different still",
+    :lab_order_genus=>"donkey",
+  }, 
+  { :patient_id=>99999, 
+    :lab_date=>"6/6/2019",
+    :lab_value=>"super",
+    :lab_order_genus=>"RPt",
+  }, 
+   { :patient_id=>99999, 
+    :lab_date=>"6/6/2019",
+    :lab_value=>"different super",
+    :lab_order_genus=>"RPt",
+  },
+  { :patient_id=>10, 
+    :lab_date=>"6/25/2019",
+    :lab_value=>"choom choom",
+    :lab_order_genus=>"  RPz",
+    :scooner=>42
+  }]
+
+
+  data.map! do |h|
+    h.transform_keys { |k| k.to_s}
+  end
+
+  new_array = []
+  number = 1
+  number2 = 1
+    data.each do |h|
+      if 
+       "[#{h["patient_id"]}][\"#{h["lab_date"]}\"]" == "#{new_array.map {|hsh| hsh["patient_id"]}}#{new_array.map {|hsh| hsh["lab_date"].to_s}}"
+        h.transform_keys! do |k| 
+          if 
+             k == "lab_order_genus"
+             k = "lab_order_genus_#{number+=1}"
+          elsif 
+             k == "lab_value"
+             k = "lab_value_#{number2+=1}"
+          else 
+             k = k
+          end
+        end
+        new_array << new_array.map { |hsh| hsh.merge!(h) }
+        new_array.pop
+      else 
+        new_array << h
+      end
+    end
+
+puts new_array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# transformed keys from symbols to strings
+data.map! do |h|
+    h.transform_keys { |k| k.to_s}
+  end
+# merges hashes together using first value for shared keys
+    new_hash = {}
+    data.each do |h|
+        new_hash.merge!(h) { |k, v1, v2| v1 }
+    end
+# changes keys with particular name
+    data.map! do |h|
+        h.transform_keys do |k| 
+          if k == "lab_order_genus"
+            k = "tony"
+          else k = k
+          end
+        end
+      end
+
+
+
+
+
+
+
+
+# def new_data_hash(data)
+#   keys = []
+#   values = []
+#   data.each do |h|
+#     keys << h.select { |k, v| k == :order_name }
+#   end
+#   data.select do |hash| 
+#     values << "#{hash[:order_name]}" + ", " + " #{hash[:lab_ord_dtl]}" + " #{hash[:lab_order_type]}" 
+#   end
+#   puts keys
+#   puts values
+# end
+
+# new_data_hash(@data)
+# # desired output :lab[number] key  == values of order_name, lab_or_dtl and lab_order_type combined
+# # these values sometimes have indentical verbiage but different outputs so they probably need a key/value rather than just a key and lab_value's value being the new value
+# # :lab[number]_result key,value == :lab_value key,value
+# @new_data = [] 
+
+# #creates array that combines values of @data keys :order_name, :lab_or_dtl and :lab_order_type
+# @data.select{ |hash| new_data << "#{hash[:order_name]}" + ", " + " #{hash[:lab_ord_dtl]}" + " #{hash[:lab_order_type]}" }
+
+# puts @new_data
+
+# def new_data_hash(data)
+#   @hash = []
+#   data.each do |h|
+#    @hash << h.transform_keys { |k| k.to_s }
+#   end
+
+  
+    # v << data.select do |val| 
+    #   "#{val[:order_name]}" + ", " + " #{val[:lab_ord_dtl]}" + " #{val[:lab_order_type]}" 
+# hash.each do |n|
+#   n.transform_values do |v| 
+#     data.select do |val| 
+#      v << "#{val[:order_name]}" + ", " + " #{val[:lab_ord_dtl]}" + " #{val[:lab_order_type]}" 
+#     end
+#   end
+# end
+# puts array_hashes
+# end
+
+# new_data_hash(@data)
+
+# puts @hash
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @data =   [{ 
+#     patient_id: "10061", 
+#     lab_date: "6/27/2019",  
+#     lab_analyte: "HPV 16",  
+#     lab_value: "NEGATIVE" },
+# { 
+#     patient_id: "10061", 
+#     lab_date: "6/27/2019",
+#     lab_analyte: "HPV 18", 
+#     lab_value: "NEGATIVE" },
+# { 
+#     patient_id: "10061",
+#     lab_date: "6/27/2019",
+#     lab_analyte: "HPV HIGH RISK INTERP", 
+#     lab_value: "POSITIVE" },
+# { 
+#     patient_id: "10061",
+#     lab_date: "5/03/2019",
+#     lab_analyte: "HPV HIGH RISK INTERP", 
+#     lab_value: "POSITIVE" },
+# { 
+#     patient_id: "44",
+#     lab_date: "6/27/2019",
+#     lab_analyte: "HPV HIGH RISK INTERP",
+#     lab_value: "POSITIVE" }]
+
+
+
+# @desired_output = [{ 
+#   patient_id: "10061",
+#   lab_date: "6/27/2019",
+#   hpv_16: "NEGATIVE",
+#   hpv_18: "NEGATIVE",
+#   hpv_high_risk_interp: "POSITIVE" },
+# {
+#   patient_id: "10061",
+#   lab_date: "5/03/2019",
+#   hpv_high_risk_interp: "POSITIVE" },
+# { 
+#   patient_id: "44",
+#   lab_date: "6/27/2019",
+#   hpv_high_risk_interp: "POSITIVE" }]
+  
+
+
+# # # run code on 'data' variable to create:
+# # # snags all patient's with id 10061
+# # # @data.select {|patient| patient[:patient_id] == "10061"}
+
+# # # snags all patient's with id 10061 and lab date 6/27/2019
+# # # @data.select {|patient| patient[:patient_id] == "10061" && patient[:lab_date] == "6/27/2019"} 
+
+# # # pseudo code
+# # # 1. in each hash within the data array transform each value of lab_analyte to a key. Make the value of that new key the value of lab_value.
+
+# # # 2. create empty array "desired_output"
+# # # 3. look at first hash in "data"
+# # # If it does NOT match any hash's patient_id AND lab_date in desired 
+
+
+
+
+
+
+
+
+
+#     data =   [{ 
+#     patient_id: "10061", 
+#     lab_date: "6/27/2019",  
+#     lab_analyte: "HPV 16",  
+#     lab_value: "NEGATIVE" }, 
+#     { 
+#     patient_id: "10061", 
+#     lab_date: "6/27/2019",  
+#     lab_analyte: "HPV 18",  
+#     lab_value: "POSITIVE" }]
+
+# test_types = []
+# test_results = []
+
+#   data.each do |hash|
+#    test_types << hash[:lab_analyte]
+#    test_results << hash[:lab_value]
+#   end
+
+# puts test_types
+# puts test_results
